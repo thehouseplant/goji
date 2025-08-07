7e7161245e9b425acac887ef575fdcdb94ec1818
Sean Collins <sean.collins@outlook.com>
2025-08-06 18:53:49 -0700

    Define Configuration structure

--- main.go
+++ main.go
@@ -5,3 +5,10 @@ import "fmt"
 func main() {
 	fmt.Println("Hello world")
 }
+
+// Define Configuration structure
+type Config struct {
+	BaseURL  string `json:"base_url"`
+	Email    string `json:"email"`
+	APIToken string `json:"api_token"`
+}
